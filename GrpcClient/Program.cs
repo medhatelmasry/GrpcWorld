d4c104686d16aa645293f030fe29b33ecae9b118
Medhat Elmasry <A00127241@ad.bcit.ca>
2019-10-31 13:28:12 -0700

    CRUD

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -20,13 +20,81 @@ namespace GrpcClient
             //Console.WriteLine(reply.Message);
 
             var channel = GrpcChannel.ForAddress("https://localhost:5001");
-            var studentClient = new RemoteStudent.RemoteStudentClient(channel);
-            var studentInput = new StudentLookupModel { StudentId = 33 };
-            var studentReply = await studentClient.GetStudentInfoAsync(studentInput);
-            Console.WriteLine($"{studentReply.FirstName} {studentReply.LastName}");
+
+            //StudentModel newStudent = new StudentModel()
+            //{
+            //    FirstName = "AAAA",
+            //    LastName = "BBBB",
+            //    School = "Tourism",
+            //};
+            //await insertStudent(channel, newStudent);
+
+
+            //StudentModel updStudent = new StudentModel()
+            //{
+            //    StudentId = 56,
+            //    FirstName = "AAAA",
+            //    LastName = "ZZZ",
+            //    School = "Medicine",
+            //};
+            //await updateStudent(channel, updStudent);
+
+            //await deleteStudent(channel, 56);
+
+            await displayAllStudents(channel);
 
             Console.ReadLine();
         }
+
+        static async Task findStudentById(GrpcChannel channel, int id)
+        {
+            var client = new RemoteStudent.RemoteStudentClient(channel);
+
+            var input = new StudentLookupModel { StudentId = id };
+            var reply = await client.GetStudentInfoAsync(input);
+            Console.WriteLine($"{reply.FirstName} {reply.LastName}");
+        }
+
+        static async Task insertStudent(GrpcChannel channel, StudentModel student)
+        {
+            var client = new RemoteStudent.RemoteStudentClient(channel);
+
+            var reply = await client.InsertStudentAsync(student);
+            Console.WriteLine(reply.Result);
+        }
+
+        static async Task updateStudent(GrpcChannel channel, StudentModel student)
+        {
+            var client = new RemoteStudent.RemoteStudentClient(channel);
+
+            var reply = await client.UpdateStudentAsync(student);
+            Console.WriteLine(reply.Result);
+        }
+
+        static async Task deleteStudent(GrpcChannel channel, int id)
+        {
+            var client = new RemoteStudent.RemoteStudentClient(channel);
+
+            var input = new StudentLookupModel { StudentId = id };
+            var reply = await client.DeleteStudentAsync(input);
+            Console.WriteLine(reply.Result);
+        }
+
+        static async Task displayAllStudents(GrpcChannel channel)
+        {
+            var client = new RemoteStudent.RemoteStudentClient(channel);
+
+            var empty = new Empty();
+            var list = await client.RetrieveAllStudentsAsync(empty);
+
+            Console.WriteLine(">>>>>>>>>>>>>>>>>>++++++++++++<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+
+            foreach (var item in list.Items)
+            {
+                Console.WriteLine($"{item.StudentId}: {item.FirstName} {item.LastName}");
+            }
+        }
+
 
     }
 }
--- GrpcService/Services/StudentsService.cs
+++ GrpcService/Services/StudentsService.cs
@@ -1,5 +1,6 @@
 ﻿using Grpc.Core;
 using GrpcService.Data;
+using GrpcService.Models;
 using GrpcService.Protos;
 using Microsoft.Extensions.Logging;
 using System;
@@ -39,6 +40,156 @@ namespace GrpcService.Services
 
             return Task.FromResult(output);
         }
-    }
+        public override Task<Reply> InsertStudent(StudentModel request, ServerCallContext context)
+        {
+            var s = _context.Students.Find(request.StudentId);
 
+            if (s != null)
+            {
+                return Task.FromResult(
+                  new Reply()
+                  {
+                      Result = $"Student {request.FirstName} {request.LastName} already exists.",
+                      IsOk = false
+                  }
+                );
+            }
+
+            Student student = new Student()
+            {
+                StudentId = request.StudentId,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                School = request.School,
+            };
+
+            _logger.LogInformation("Insert student");
+
+            try
+            {
+                _context.Students.Add(student);
+                var returnVal = _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation(ex.ToString());
+            }
+
+            return Task.FromResult(
+               new Reply()
+               {
+                   Result = $"Student {request.FirstName} {request.LastName}  was successfully inserted.",
+                   IsOk = true
+               }
+            );
+        }
+
+        public override Task<Reply> UpdateStudent(StudentModel request, ServerCallContext context)
+        {
+            var s = _context.Students.Find(request.StudentId);
+
+            if (s == null)
+            {
+                return Task.FromResult(
+                  new Reply()
+                  {
+                      Result = $"Student {request.FirstName} {request.LastName} cannot be found.",
+                      IsOk = false
+                  }
+                );
+            }
+
+            s.FirstName = request.FirstName;
+            s.LastName = request.LastName;
+            s.School = request.School;
+
+            _logger.LogInformation("Update student");
+
+            try
+            {
+                var returnVal = _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation(ex.ToString());
+            }
+
+            return Task.FromResult(
+               new Reply()
+               {
+                   Result = $"Student {request.FirstName} {request.LastName} was successfully updated.",
+                   IsOk = true
+               }
+            );
+        }
+
+        public override Task<Reply> DeleteStudent(StudentLookupModel request, ServerCallContext context)
+        {
+            var s = _context.Students.Find(request.StudentId);
+
+            if (s == null)
+            {
+                return Task.FromResult(
+                  new Reply()
+                  {
+                      Result = $"Student with ID {request.StudentId} cannot be found.",
+                      IsOk = false
+                  }
+                );
+            }
+
+            _logger.LogInformation("Delete Student");
+
+            try
+            {
+                _context.Students.Remove(s);
+                var returnVal = _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation(ex.ToString());
+            }
+
+            return Task.FromResult(
+               new Reply()
+               {
+                   Result = $"Student with ID {request.StudentId} was successfully deleted.",
+                   IsOk = true
+               }
+            );
+        }
+
+        public override Task<StudentList> RetrieveAllStudents(Empty request, ServerCallContext context)
+        {
+            _logger.LogInformation("Retrieving all students");
+
+            StudentList list = new StudentList();
+
+            try
+            {
+                List<StudentModel> studentList = new List<StudentModel>();
+
+                var students = _context.Students.ToList();
+
+                foreach (var c in students)
+                {
+                    studentList.Add(new StudentModel()
+                    {
+                        StudentId = c.StudentId,
+                        FirstName = c.FirstName,
+                        LastName = c.LastName,
+                        School = c.School,
+                    });
+                }
+
+                list.Items.AddRange(studentList);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation(ex.ToString());
+            }
+
+            return Task.FromResult(list);
+        }
+    }
 }
